d6230077bc826d94a0df73c938ce6620b15c9e86
Dung Tien The <dungtienthe1920@gmail.com>
2024-10-14 16:28:58 +0700

    d

---
+++ BaiThucHanh2/WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebBanHang.Models;
+using X.PagedList;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        QLBanVaLiContext db = new QLBanVaLiContext();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int ?page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstSanPham = db.TDanhMucSps.AsNoTracking().OrderBy(x=>x.TenSp);
+            
+            //TDanhMucSp là bảng SanPham
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstSanPham, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        public IActionResult SanPhamTheoLoai(string maloai, int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstSanPham = db.TDanhMucSps.AsNoTracking().Where(x=>x.MaLoai==maloai).OrderBy(x => x.TenSp);
+
+            ViewBag.maloai = maloai;
+            //TDanhMucSp là bảng SanPham
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstSanPham, pageNumber, pageSize);
+            return View(lst);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
